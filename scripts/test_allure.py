d0bd66b1551271ceedb4ef6c37bea2f724923210
zhaowenjuanSunshine <18710971874@163.com>
2019-06-12 17:10:11 +0800

    Initial commit

---
+++ scripts/test_allure.py
@@ -0,0 +1,8 @@
+# import pytest
+class Test_ABC:
+    def test_001(self):
+        print(">>>>>>>>test_001>>>>>>>>>")
+        assert 1
+# if __name__ == '__main__':
+    # pytest.main("-s test_allure.py")
+    # pytest.main("-s --alluredir report  test_001.py")
